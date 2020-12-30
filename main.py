aa39964f8de795b631d6a0d8499c20a8ad298975
taroserigano <taroserigano@gmail.com>
2020-12-30 11:00:19 +0100

    mnfg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -9,12 +10,25 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
+mail = Mail(app)
+
+#for email setting
+app.config['SECRET_KEY'] = 'top-secret!'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'kim.13charls@gmail.com'
+app.config['MAIL_PASSWORD'] = 'tarodesu123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
@@ -161,10 +175,21 @@ def about():
     return render_template("about.html", current_user=current_user)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html", current_user=current_user)
 
+    if request.method == 'POST':
+        recipient = request.form['recipient']
+        name= request.form['name']
+        phone = request.form['phone']
+        recipient = request.form['recipient']
+        message = request.form['message']
+        msg = Message('New email from the Blog Website!', sender='kim.13charls@gmail.com', recipients=[recipient])
+        msg.body = (f"Name: {name} \nPhone Number: {phone} \nEmail {recipient} \nMessage: {message}")
+        mail.send(msg)
+        flash(f'Thanks! Your email was successfully sent to {recipient}!')
+        return redirect(url_for('contact'))
+    return render_template("contact2.html", current_user=current_user)
 
 @app.route("/new-post", methods=["GET", "POST"])
 @admin_only
